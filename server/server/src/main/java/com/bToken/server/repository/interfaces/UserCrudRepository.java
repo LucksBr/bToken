0b8eb6a1db3028c2da8c43cc6ff262702503e16b
LucksBr <lucas0405prado@gmail.com>
2025-09-08 23:52:09 -0300

    Added dependency spring-boot-starter-security

--- server/server/src/main/java/com/bToken/server/repository/interfaces/UserCrudRepository.java
+++ server/server/src/main/java/com/bToken/server/repository/interfaces/UserCrudRepository.java
@@ -1,6 +1,10 @@
 package com.bToken.server.repository.interfaces;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.bToken.server.model.User;
@@ -8,4 +12,7 @@ import com.bToken.server.model.User;
 @Repository
 public interface UserCrudRepository extends JpaRepository<User,Integer> {
     
+    @Query("SELECT * FROM User u WHERE u.email = :email")
+    Optional<User> findByEmail(@Param("email") String email);
+
 }
