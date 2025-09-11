2e10c2ee0116c2b7bba0279f4f3ac1679726d0ef
LucksBr <lucas0405prado@gmail.com>
2025-09-11 00:34:40 -0300

    Added override validate method to UserGroupTokenServiceImpl

--- server/server/src/main/java/com/bToken/server/service/config/UserGroupTokenServiceImpl.java
+++ server/server/src/main/java/com/bToken/server/service/config/UserGroupTokenServiceImpl.java
@@ -6,16 +6,33 @@ import com.bToken.server.service.custom.AbstractCrudService;
 import com.bToken.server.service.custom.ValidatorEntity;
 import com.bToken.server.service.custom.ValidatorField;
 import com.bToken.server.service.interfaces.UserGroupTokenService;
+import com.bToken.server.service.interfaces.UserTokenService;
+import com.bToken.server.service.utils.CollectionUtils;
+import com.bToken.server.service.validation.ServiceException;
 
 public class UserGroupTokenServiceImpl extends AbstractCrudService<UserGroupToken, Integer> implements UserGroupTokenService {
 
-    public UserGroupTokenServiceImpl(UserGroupTokenRepository repository) {
+    private final UserTokenService userTokenService;
+
+    public UserGroupTokenServiceImpl(UserGroupTokenRepository repository,UserTokenService userTokenService) {
         super(repository);
+        this.userTokenService = userTokenService;
 
         setValidatorEntity(new ValidatorEntity<>(getErrorBuilder(),
              new ValidatorField("user","O usuário deve ser informado!"),
              new ValidatorField("groupToken","O grupo deve ser informado!")
         ));
+    }
+
+    @Override
+    public void validate(UserGroupToken entity) throws ServiceException {
+        super.validate(entity);
+
+        if(CollectionUtils.isEmpty(entity.getUserTokenList())){
+            getErrorBuilder().addError("O grupo deve possuir pelo menos um token!");
+        }else{
+            entity.getUserTokenList().stream().forEach( userToken -> userTokenService.validate(userToken));
+        }
     }
     
 }
